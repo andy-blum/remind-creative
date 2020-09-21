95504eebd46a2814d88d5ac1f38d1a32a7668b5c
Andy Blum <andy@gravityworksdesign.com>
2020-09-20 21:33:04 -0400

    fix animsition on safari (?)

--- themes/custom/remind/src/js/pageTransition.js
+++ themes/custom/remind/src/js/pageTransition.js
@@ -1,21 +1,20 @@
 (function ($) {
-  Drupal.behaviors.animsition = {
-    attach: function (context, settings) {
-      $(document).ready(function () {
-        $(".animsition").animsition({
-          inClass: 'fade-in-down-sm',
-          outClass: 'fade-out-up-sm',
-          inDuration: 1500,
-          outDuration: 800,
-          linkElement: 'a:not([target="_blank"]):not([href^="#"]):not([href=""])',
-          onLoadEvent: true,
-          browser: ['animation-duration', '-webkit-animation-duration'],
-          // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
-          // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
-          overlay: false,
-          transition: function (url) { window.location.href = url; }
-        });
-      });
-    }
-  };
+  console.log('page transition behavior');
+  debugger;
+  $(document).ready(function () {
+    $(".animsition").animsition({
+      inClass: 'fade-in-down-sm',
+      outClass: 'fade-out-up-sm',
+      inDuration: 1500,
+      outDuration: 800,
+      linkElement: 'a:not([target="_blank"]):not([href^="#"]):not([href=""])',
+      loading: false,
+      onLoadEvent: true,
+      browser: ['animation-duration', '-webkit-animation-duration'],
+      // "browser" option allows you to disable the "animsition" in case the css property in the array is not supported by your browser.
+      // The default setting is to disable the "animsition" in a browser that does not support "animation-duration".
+      overlay: false,
+      transition: function (url) { window.location.href = url; }
+    });
+  });
 })(jQuery);
